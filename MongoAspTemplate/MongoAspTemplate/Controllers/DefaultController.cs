2aa91dad3a2bf87c0f7c76ee9930a729a5576cb2
jmistx <blohin.o.d@gmail.com>
2016-01-14 04:20:27 +0300

    Add work with db

--- MongoAspTemplate/MongoAspTemplate/Controllers/DefaultController.cs
+++ MongoAspTemplate/MongoAspTemplate/Controllers/DefaultController.cs
@@ -1,18 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoAspTemplate.Controllers
 {
     public class DefaultController : Controller
     {
+        protected IMongoClient _client;
+        protected IMongoDatabase _database;
+
+        public DefaultController() {
+            _client = new MongoClient(new MongoClientSettings
+            {
+                Server = new MongoServerAddress("localhost", 27017)
+            });
+            _database = _client.GetDatabase("test");
+        }
+
         //
         // GET: /Default/
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            var resourants = await collection.Find(new BsonDocument()).ToListAsync();
+
+            return View(resourants);
+        }
+
+        public async Task<RedirectToRouteResult> New() {
+            var document = new BsonDocument() {
+                {
+                    "address", new BsonDocument {
+                        {"street", "2 Avenue"},
+                        {"zipcode", "10075"},
+                        {"building", "1480"},
+                        {"coord", new BsonArray {73.9557413, 40.7720266}}
+                    }
+                },
+                { "borough", "Manhattan" },
+                { "cuisine", "Italian" }
+            };
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            await collection.InsertOneAsync(document);
+
+            return RedirectToAction("Index");
         }
 	}
 }
